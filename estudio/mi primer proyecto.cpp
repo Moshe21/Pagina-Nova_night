77dee4a20ef8fe1dd23e8a6661f3b3672314076e
Moshe <83256516+Moshe21@users.noreply.github.com>
2023-03-28 19:44:35 -0500

    carpeta de estudio
    para guadar los programas de  pc clase

---
+++ estudio/mi primer proyecto.cpp
@@ -0,0 +1,23 @@
+/* mi primer programa lenguaje c*/
+
+#include <stdio.h>
+/* declarar funcion*/
+int main(){
+	/* ingresando varible*/
+	int num=89;
+	/*condicional inicial "mayor que"*/
+	if(num>=18){
+		///utilizando una instruccion
+		printf("you are eligible for voting");
+	}
+	else{
+		printf("Error... no eres mayor")
+	}
+		
+		
+		
+	
+		return 0;
+}
+
+
